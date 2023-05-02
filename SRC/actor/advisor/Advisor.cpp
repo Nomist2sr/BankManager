5bfc89b5ee57e1f6e12c52391e0b9ee76b60c97d
Nomist2sr <simon.tessier@epitech.eu>
2023-05-02 17:28:56 +0300

    [MODIFY] remove end of file empty lines

--- SRC/actor/advisor/Advisor.cpp
+++ SRC/actor/advisor/Advisor.cpp
@@ -4,11 +4,9 @@
 
 using namespace std;
 
-//Implémentation de la classe Advisor
+//Implï¿½mentation de la classe Advisor
 
 
 Advisor::Advisor(string name, string firstname, string adress) :Actor(name, firstname, adress) {};
 
 Advisor::~Advisor() {};
-
-
