0d349f02359bc8bd97163c8dbd77a79359422d59
JoachimDSP <jo.de-sousa-peixeira@laposte.net>
2023-05-02 15:04:02 +0200

    Add Actor, Advisor and Customer headers files

--- SRC/actor/Actor.hpp
+++ SRC/actor/Actor.hpp
@@ -10,6 +10,23 @@ class Actor {
 private:
 
 public:
-
+	// Constructeur
+	Actor();
+	Actor(string name, string firstname, string adress);
+	// Destructeur
+	~Actor();
+	// Getter
+	string getName() const;
+	string getFirstname() const;
+	string getAdress() const;
+	// Setter
+	void setName(string const name);
+	void setFirstname(string const firstname);
+	void setAdress(string const adress);
+	// Méthode
+	string displayInformation();
 protected:
+	string name;
+	string firstname;
+	string adress;
 };
---
+++ SRC/actor/advisor/Advisor.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../Actor.hpp"
+
+using namespace std;
+
+class Adivsor : Actor {
+private:
+
+public:
+	// Constructeur
+	Adivsor();
+	Adivsor(string name, string firstname, string adress);
+	// Destructeur
+	~Adivsor();
+	// Méthode abstraite implémentée
+
+protected:
+	
+};
