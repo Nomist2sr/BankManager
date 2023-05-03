00d94962bcb5510a4d5bf1b4f9d4bb73ddf84cb5
Gu21 <bisson_guillaume@yahoo.fr>
2023-05-03 09:09:47 +0200

    change main.cpp

--- SRC/main/Main.cpp
+++ SRC/main/Main.cpp
@@ -1,7 +1,21 @@
+#include <string>
+#include <iostream>
 #include "Main.hpp"
+#include "../actor/Actor.hpp"
+#include "../account/Account.hpp"
+#include "../transaction/Transaction.hpp"
+#include "../management/Management.hpp"
 
-int main(void) {
-    cout << "YEET" << endl;
+int main() {
+    
+
+    
+
+    
+
+
+
+    
 
     return (0);
 }
